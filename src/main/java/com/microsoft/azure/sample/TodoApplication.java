127f06cbecbd1a82a78c5dfb85ec8382546a3aa6
yanzh <yanzh@microsoft.com>
2017-11-29 15:33:57 +0800

    war

--- src/main/java/com/microsoft/azure/sample/TodoApplication.java
+++ src/main/java/com/microsoft/azure/sample/TodoApplication.java
@@ -7,9 +7,10 @@ package com.microsoft.azure.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
